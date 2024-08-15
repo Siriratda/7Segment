8698f918df0f2015ad87d964a3081d4a9442fdc6
Siriratda Wangsatang <65030240@kmitl.ac.th>
2024-08-15 15:23:35 +0700

    update

--- main/main.cpp
+++ main/main.cpp
@@ -6,15 +6,54 @@ SevenSegment s2(4) ;
 
 extern "C" void app_main(void)
 {
+    printf("Start...\r\n");
     while(1)
     {
-        s1.HardwareTest();
+
+        printf("Digit 1\r\n");
+        s1.DisplayNum0();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s1.DisplayNum1();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s1.DisplayNum2();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s1.DisplayNum3();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s1.DisplayNum4();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s1.DisplayNum5();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s1.DisplayNum6();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s1.DisplayNum7();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s1.DisplayNum8();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s1.DisplayNum9();
         vTaskDelay(500/portTICK_PERIOD_MS);
         s1.DisplayBlank();
+
+        printf("Digit 2\r\n");
+        s2.DisplayNum0();
         vTaskDelay(500/portTICK_PERIOD_MS);
-        s2.HardwareTest();
+        s2.DisplayNum1();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s2.DisplayNum2();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s2.DisplayNum3();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s2.DisplayNum4();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s2.DisplayNum5();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s2.DisplayNum6();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s2.DisplayNum7();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s2.DisplayNum8();
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        s2.DisplayNum9();
         vTaskDelay(500/portTICK_PERIOD_MS);
         s2.DisplayBlank();
-        vTaskDelay(500/portTICK_PERIOD_MS);
     } 
 }
